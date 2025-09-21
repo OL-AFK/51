1486c5f8b2fd8fb16211b8d60b89cccb01748cf1
Liu QI <1810401980@qq.com>
2025-09-21 12:01:58 +0800

    Add files via upload

---
+++ Seg/main.c
@@ -0,0 +1,60 @@
+ #include <REGX52.H>
+
+unsigned char Seg_Dei[] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
+
+void Nixie(unsigned char wei,dei)
+{
+	switch(wei)
+	{
+		case 1:
+			P2_4 = 1;
+			P2_3 = 1;
+			P2_2 = 1;
+		break;
+		case 2:
+			P2_4 = 1;
+			P2_3 = 1;
+			P2_2 = 0;
+		break;
+		case 3:
+			P2_4 = 1;
+			P2_3 = 0;
+			P2_2 = 1;
+		break;
+		case 4:
+			P2_4 = 1;
+			P2_3 = 0;
+			P2_2 = 0;
+		break;
+		case 5:
+			P2_4 = 0;
+			P2_3 = 1;
+			P2_2 = 1;
+		break;
+		case 6:
+			P2_4 = 0;
+			P2_3 = 1;
+			P2_2 = 0;
+		break;
+		case 7:
+			P2_4 = 0;
+			P2_3 = 0;
+			P2_2 = 1;
+		break;
+		case 8:
+			P2_4 = 0;
+			P2_3 = 0;
+			P2_2 = 0;
+		break;
+	}
+	P0 = Seg_Dei[dei];
+}
+
+int main()
+{
+	Nixie(2,4);
+	while(1)
+	{
+		
+	}
+}
